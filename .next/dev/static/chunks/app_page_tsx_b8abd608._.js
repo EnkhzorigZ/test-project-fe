350c143f7131c20c76424edbd6228c972a3e428c
Enkhzorigz <Enkhzorig@coremind.mn>
2026-01-09 09:01:19 +0800

    changes

--- .next/dev/static/chunks/app_page_tsx_b8abd608._.js
+++ .next/dev/static/chunks/app_page_tsx_b8abd608._.js
@@ -1,8 +1,10 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_1fb089b4._.js",
-  "static/chunks/node_modules_01c6a71d._.js"
+  "static/chunks/_47f562ce._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_53b8cc60._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_d176de27._.js",
+  "static/chunks/node_modules_432fd8db._.js"
 ],
     source: "dynamic"
 });
